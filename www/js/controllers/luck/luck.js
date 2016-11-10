26445771696b9301db77f6f855d7bd0a60129dcc
Aleksey Pivkin <apivkin@at-consulting.ru>
2016-11-10 13:48:24 +0300

    luky page 1

--- www/js/controllers/luck/luck.js
+++ www/js/controllers/luck/luck.js
@@ -4,11 +4,19 @@ angular.module('app.controllers.luck', [])
     function($scope, $stateParams, API) {
         $scope.option = {
             count: 2,
-            price_zone: 0,
-            alco_zone: 1,
+            price_zone: 0, // 0 - дешево, 1 - средне, 2 - дорого
+            alco_zone: 1,  // 0 - без алко, 1 - легкие, 2 - крепкие
             snaks: false
         };
 
+        $scope.list = [];
 
+
+        $scope.get = function(){
+            API.getOrder($scope.option)
+            .then(function(res){
+                //
+            })
+        };
     }
 ]);
--- www/js/services.js
+++ www/js/services.js
@@ -2,10 +2,57 @@ angular.module('app.services', [])
 
 .factory('API', ['$http', function($http) {
     var url = 'http://localhost:30303';
-    
+
     return {
-        getBars: function(){
+        /**
+         * получение списка баров
+         */
+        getBars: function() {
             return $http.get(url + '/bars')
+        },
+
+        /**
+         * получение заказа
+         * 
+         * @params object params объект с параметрами запроса 
+         */
+        getOrder: function(params) {
+            var options = {
+                params: params
+            };
+
+            return $http.get(url + '/bars', options)
+        }
+    };
+}])
+
+/**
+ * проставляет нужный класс в случае верности условия
+ * 
+ * @example
+ * <button is-active="value==ture" cl="btn-info">Go</button>
+ */
+.directive('isActive', [function() {
+    return {
+        restrict: 'A',
+        scope: {
+            isActive: '='
+        },
+
+        link: function(scope, el, attrs) {
+
+            scope.$watch('isActive', (function(nw, ow) {
+                // if (nw === ow) return;
+
+                var className = this.attr('cl') || 'active';
+
+                if(!!nw){
+                    this.addClass(className);
+                }else{
+                    this.removeClass(className); 
+                }
+
+            }).bind(el));
         }
     };
 }])
